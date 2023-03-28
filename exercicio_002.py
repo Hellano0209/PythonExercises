4651d3adb402c5ddcae4e56378ae44c9d114498f
Hellano0209 <83433027+Hellano0209@users.noreply.github.com>
2023-03-28 10:55:07 -0300

    Add files via upload

---
+++ exercicio_002.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 15 15:54:41 2023
+
+@author: hellano
+"""
+
+nome = input('Oi seja bem-vindo! Qual o seu nome: ')
+nome = nome.capitalize()
+
+print('Oi {} é um prazer te conhecer!'.format(nome))
