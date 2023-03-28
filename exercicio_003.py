745f29630ddb3c14a4b9fe70fd242f18736666c8
Hellano0209 <83433027+Hellano0209@users.noreply.github.com>
2023-03-28 10:55:31 -0300

    Add files via upload

---
+++ exercicio_003.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 20 14:22:28 2023
+
+@author: hellano
+"""
+
+n1 = float(input("Digite um número: "))
+n2 = float(input("Digite um segundo número "))
+
+print('A soma entre {} e {} é igual a {}.'.format(n1, n2, n1+n2))
