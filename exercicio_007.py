5d1a26d761c362a79b94a794da0e77b7bae47a84
Hellano0209 <83433027+Hellano0209@users.noreply.github.com>
2023-03-28 10:56:15 -0300

    Add files via upload

---
+++ exercicio_007.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 21 11:14:45 2023
+
+@author: hellano
+"""
+
+n1 = float(input("Valor da primeira nota: "))
+n2 = float(input("Valor da segunda nota: "))
+
+print("\nA sua nota média é {}.".format((n1 + n2)/2))
