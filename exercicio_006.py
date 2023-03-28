255941ab54efc1fe702c23c66f1bba0c3e4c07c2
Hellano0209 <83433027+Hellano0209@users.noreply.github.com>
2023-03-28 10:56:06 -0300

    Add files via upload

---
+++ exercicio_006.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 21 10:58:59 2023
+
+@author: hellano
+"""
+
+import math
+n = float(input(" Digite um número: "))
+print(" O dobro de {} é {}.\n O triplo de {} é {}.\n A raíz quadrada de {} é {}.".format(n, 2*n, n, 3*n, n, math.sqrt(n)))
