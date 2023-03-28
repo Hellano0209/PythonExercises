923e5012d1331145584424119cc4219cfa768664
Hellano0209 <83433027+Hellano0209@users.noreply.github.com>
2023-03-28 10:55:54 -0300

    Add files via upload

---
+++ exercicio_005.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 21 10:47:41 2023
+
+@author: hellano
+"""
+
+n = int(input("Digite um número inteiro: "))
+print("Analizando o número {}, o seu antecessor é {} e o seu sucessor é {}.".format(n, n-1, n+1))
